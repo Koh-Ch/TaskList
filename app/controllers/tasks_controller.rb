956ca7860a54bc9395927f4a2d7be51773ba3117
koh <ec2-user@ip-172-31-1-214.us-east-2.compute.internal>
2018-03-07 05:00:04 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,8 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit,:destroy]
+
 
   def index
     @tasks=Task.all.page(params[:page])
@@ -20,7 +23,8 @@ class TasksController < ApplicationController
       redirect_to @task
     else
       flash.now[:danger] = 'Task が投稿されませんでした'
-      render :new
+      #render :new
+      render 'toppages/index'
     end
   end
 
@@ -53,6 +57,13 @@ class TasksController < ApplicationController
   
   def set_task
     @task = Task.find(params[:id])
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
   
 end
