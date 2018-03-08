eaeada5dcbe97be40ebc4b827f3970af70d57055
koh <ec2-user@ip-172-31-1-214.us-east-2.compute.internal>
2018-03-08 14:57:28 +0000

    root

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,8 +19,8 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      #redirect_to users_url
-      redirect_to root_url
+      redirect_to users_url
+      #redirect_to root_url
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
